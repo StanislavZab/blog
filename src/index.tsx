004d719b26e421d8b5f95434fa790918e37d714d
Stanislav <hrustik-st@bk.ru>
2023-02-21 21:48:05 +0500

    add page loader, not founder page,  error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import App from 'app/app';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 
 import 'shared/config/i18n/i18n';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
 );
