90e9ee571db7d5cdcd20fde547123836465fdb65
Stanislav <hrustik-st@bk.ru>
2023-02-23 19:48:00 +0500

    fix jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,16 +1,6 @@
 import path from 'path';
 
 export default {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\Станислав\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls, instances, contexts and results before every test
     clearMocks: true,
 
     testEnvironment: 'jsdom',
@@ -52,5 +42,7 @@ export default {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
+
+    preset: 'ts-jest',
 
 };
